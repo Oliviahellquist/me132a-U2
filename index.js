0a4eea8d8e971cb449d05aff8310df2860f7b2e6
Oliviahellquist <97952203+Oliviahellquist@users.noreply.github.com>
2022-02-28 09:53:29 +0100

    index.js = en array med snittblommor

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+//global variabel 
+//En array med olika snittblommor 
+let webbside = [
+    {
+        id: 1,
+        name: "Ranukel",
+        price: 60,
+        color: "Rosa",
+        season: "Vår"
+
+    },
+    {
+        id: 2,
+        name: "Pion",
+        price: 40,
+        color: "Vit",
+        season: "Sommar"
+    },
+    {
+        id: 3,
+        name: "Dahalia",
+        price: 60,
+        color: "Orange",
+        season: "Höst"
+    },
+    {
+        id: 4,
+        name: "Amaryllis",
+        price: 70,
+        color: "Röd",
+        season: "Vinter"
+    },
+    {
+        id: 5,
+        name: "Tulpan",
+        price: 35,
+        color: "Gul",
+        season: "Vår"
+    },
+    {
+        id: 6,
+        name: "Anemon",
+        price: 35,
+        color: "Lila",
+        season: "Vår"
+    }
+];
